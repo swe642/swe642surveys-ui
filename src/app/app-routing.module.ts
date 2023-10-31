d6c29fbcc8d65f0d850329aa94401c1caf31e840
Vamshi Gopari <vamshi.gopari@motivitylabs.com>
2023-10-31 19:53:09 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SurveysComponent } from './surveys/surveys.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full'},
+  { path: 'home', component: SurveysComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
